182122485c4403d826b3a756fedff7dafb6b0c21
tama <tama@ttk1.net>
2019-11-13 02:36:49 +0900

    オレオレjsonシリアライズ

--- src/main/java/net/ttk1/blocksplugin/BlocksHandler.java
+++ src/main/java/net/ttk1/blocksplugin/BlocksHandler.java
@@ -1,9 +1,11 @@
 package net.ttk1.blocksplugin;
 
+import org.bukkit.Chunk;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -17,15 +19,48 @@ public class BlocksHandler extends AbstractHandler {
     }
 
     @Override
-    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException {
         // log
         plugin.getLogger().info(target);
 
         // process
-        response.setContentType("text/html; charset=utf-8");
+        response.setContentType(" application/json; charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         PrintWriter writer = response.getWriter();
-        writer.println("hello, world!");
+
+        // chunkデータの送信
+        World world = plugin.getServer().getWorld("world");
+        if (world == null) {
+            writer.println("{}");
+            baseRequest.setHandled(true);
+            return;
+        }
+
+        Chunk chunk = world.getChunkAt(0, 0);
+
+        writer.print("[");
+        for (int x = 0; x < 16; x++) {
+            writer.print("[");
+            for (int y = 0; y < 256; y++) {
+                writer.print("[");
+                for (int z = 0; z < 16; z++) {
+                    Block block = chunk.getBlock(x, y, z);
+                    writer.print("\"" + block.getType() + "\"");
+                    if (z < 15) {
+                        writer.print(",");
+                    }
+                }
+                writer.print("]");
+                if (y < 255) {
+                    writer.print(",");
+                }
+            }
+            writer.print("]");
+            if (x < 15) {
+                writer.print(",");
+            }
+        }
+        writer.print("]");
         baseRequest.setHandled(true);
     }
 }
