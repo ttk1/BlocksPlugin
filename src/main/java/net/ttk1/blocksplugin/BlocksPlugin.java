188937c2243160eea507b8cb1b9bddb3ae08fafe
tama <tama@ttk1.net>
2019-11-13 22:52:10 +0900

    ポートを設定で指定できるようにした

--- src/main/java/net/ttk1/blocksplugin/BlocksPlugin.java
+++ src/main/java/net/ttk1/blocksplugin/BlocksPlugin.java
@@ -1,5 +1,6 @@
 package net.ttk1.blocksplugin;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.eclipse.jetty.server.Server;
 
@@ -8,9 +9,14 @@ public class BlocksPlugin extends JavaPlugin {
     public void onEnable() {
         getLogger().info("Hi!");
 
+        // config
+        saveDefaultConfig();
+        FileConfiguration config = this.getConfig();
+        int port = config.getInt("port", 9000);
+
         // Start JettyServer!
         getLogger().info("starting jetty server...");
-        Server server = new Server(3000);
+        Server server = new Server(port);
         server.setHandler(new BlocksHandler(this));
         try {
             server.start();
